3f12d37f954dac94fde40a5ae2849aa1f5882c1b
Viktor Morhun <morhun.fe@gmail.com>
2024-09-24 18:54:11 +0300

    update link

--- assets/fetchData.js
+++ assets/fetchData.js
@@ -1,6 +1,6 @@
 export async function fetchData() {
   try {
-    const dataLink = 'https://api.openweathermap.org/data/2.5/weather?lat=49.06&lon=33.34&appid=c1abf316beae57a6d1cfc5df2ac0f7a4';
+    const dataLink = 'https://api.openweathermap.org/data/2.5/weather?lat=49.06&lon=33.34&appid={apiKey}';
     const response = await fetch(dataLink);
 
     if (!response.ok) {
